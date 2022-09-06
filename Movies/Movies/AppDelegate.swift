17ec6267593dd90222f5f19e9de0ba1b8be53c73
Dikran Teymur <mdteymur@gmail.com>
2022-09-06 19:05:44 +0300

    First Commit
    Project cleaned

---
+++ Movies/Movies/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Movies
+//
+//  Created by Dikran Teymur on 6.09.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+
+
+
+}
+
